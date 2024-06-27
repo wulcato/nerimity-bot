7c76d9c01688e36fbc48994de2ef487a619ee693
Supertiger <saawalkhn@googlemail.com>
2024-06-27 11:22:04 +0100

    Add html embed

--- src/index.js
+++ src/index.js
@@ -27,18 +27,22 @@ bot.on("messageCreate", async (message) => {
     await addXp(message.user.id, message.channel.serverId, message.user.username);
 
     if (message?.content === cmd("ping")) {
-        return message.reply("Pong!");
+        const t0 = Date.now();
+        return message.reply("Pong!").then(m => m.edit(`${m.content} (${Date.now() - t0}ms)`));
     }
 
     const args = message.content?.split?.(" ") || [];
 
     if (args[0] === cmd("profile")) {
-        return profileCmd(message)
+        return profileCmd(message, "server")
+    }
+    if (args[0] === cmd("globalProfile")) {
+        return profileCmd(message, "user")
     }
 
-    if (args[0] === cmd("chat")) {
-        const msg = args.slice(1).join(" ");
-    }
+    // if (args[0] === cmd("chat")) {
+    //     const msg = args.slice(1).join(" ");
+    // }
 })
 
 
@@ -47,31 +51,95 @@ bot.on("messageCreate", async (message) => {
  *
  * @param {import("@nerimity/nerimity.js/build/Client.js").Message} message - The message object containing the user's command.
  */
-const profileCmd = async (message) => {
+const profileCmd = async (message, profile) => {
 
     const args = message.content.split(" ");
     const userId = args[1] || message.user.id;
 
     let user = await getUser(userId);
+    
     const server = await getServer(message.channel.serverId, userId)
+
     if (!user) {
         message.reply("User not found.");
         return;
     }
+    if (profile === "server" && !server) {
+        message.reply("User not found.");
+        return;
+    }
 
-const serverProfile = server ? `**Server Profile**
-XP: ${server.xp}/${calculateRequiredXp(server.level)}
-Level: ${server.level}` : '';
-
-message.channel.send(`${serverProfile}
-
-**Global Profile**
-XP: ${user.xp}/${calculateRequiredXp(user.level)}
-Level: ${user.level}
-`)
+    message.reply("", {
+        htmlEmbed: htmlProfileBuilder(server, user, profile)
+    })
 
 
 }
 
+
+
+
+/**
+ * Builds an HTML profile based on the provided server and user information.
+ *
+ * @param {import('@prisma/client').Server} server - The server information.
+ * @param {import('@prisma/client').User} user - The user information.
+ * @param {"server" | "user"} profile 
+ * @return {string} The HTML profile.
+ */
+const htmlProfileBuilder = (server, user, profile) => {
+
+    const level = profile === "server" ? server.level : user.level
+    const currentXP = profile === "server" ? server.xp : user.xp;
+    const xpRequired = profile === "server" ? calculateRequiredXp(server.level) : calculateRequiredXp(user.level);
+    const percent = (currentXP / xpRequired) * 100;
+
+    return `
+        <div class="ctn">
+            <div class="h">${user.username}<div class="sh">(${profile === "server" ? 'Server' : 'User'} Profile)</div></div>
+            <div class="t">Level ${level}</div>
+            <div class="t">${currentXP}/${xpRequired}</div>
+            <div class="b"><div style="width: ${percent}%"></div></div>
+
+        </div>
+        <style>
+            .ctn {
+                font-family: monospace;
+                background-color: #000;
+                margin: 6px;
+                text-align: center;
+                border-radius: 6px;
+                overflow: hidden;
+            }
+            .h {
+                background-color: var(--primary-color);
+                padding: 6px;
+                margin-bottom: 6px;
+            }
+            .sh {
+                font-size: 12px;
+                color: #ffffff9c;
+            }
+            .t {
+                margin-left: 6px;
+                margin-right: 6px;
+            }
+            .b {
+                background-color: #000;
+                margin-top: 2px;
+                border-radius: 4px;
+                border: 1px solid #fff;
+                padding: 2px;
+                margin: 6px;
+            }
+            .b div {
+                border-radius: 2px;
+                height: 8px;
+                background-color: #fff;
+            }
+        </style>
+    `
+
+}
 
 bot.login(config.token);
