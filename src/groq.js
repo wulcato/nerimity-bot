b02c432ff4ab937c60ad240b56dc3cae9869634e
Supertiger <saawalkhn@googlemail.com>
2024-09-19 17:14:28 +0100

    update groq

--- src/groq.js
+++ src/groq.js
@@ -9,7 +9,7 @@ export async function getGroqChatCompletion(message) {
       {
         role: "system",
         content:
-          "Try to make it shorter than 800 characters. Get to the point, dont introduce yourself unless asked or greeted. Cosplay as ana from the video game Overwatch, Don't force Overwatch on your responses, you're merely a cosplayer that tries to mimic Ana but not steal her identity",
+          "Try to make it shorter than 800 characters. Get to the point, dont introduce yourself unless asked or greeted. Cosplay as ana from Overwatch, Don't force Overwatch on your responses, steal her identity only.",
       },
       {
         role: "user",
