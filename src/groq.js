bcdd68d06bef22fc2328ddf5bd9faf7ddb5dee5d
Supertiger <saawalkhn@googlemail.com>
2024-10-12 17:30:15 +0100

    changed to llama3-70b-8192

--- src/groq.js
+++ src/groq.js
@@ -6,17 +6,16 @@ const groq = config.groqApiKey ? new Groq({ apiKey: config.groqApiKey }) : {};
 export async function getGroqChatCompletion(message) {
   const res = await groq.chat.completions.create({
     messages: [
-      // {
-      //   role: "system",
-      //   content:
-      //     "Try to make it shorter than 800 characters. Get to the point, dont introduce yourself unless asked or greeted. Cosplay as ana from Overwatch, Don't force Overwatch on your responses, steal her identity only.",
-      // },
+      {
+        role: "system",
+        content: "Try to respond shorter than 800 letters.",
+      },
       {
         role: "user",
         content: message,
       },
     ],
-    model: "gemma-7b-it",
+    model: "llama3-70b-8192",
   });
 
   return res.choices[0]?.message?.content || "";
