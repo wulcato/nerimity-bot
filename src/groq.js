a4a3fb5068fdf326fa346fa7c999d063dcc02c3f
Supertiger <saawalkhn@googlemail.com>
2024-10-12 17:20:44 +0100

    remove system role from groq

--- src/groq.js
+++ src/groq.js
@@ -6,11 +6,11 @@ const groq = config.groqApiKey ? new Groq({ apiKey: config.groqApiKey }) : {};
 export async function getGroqChatCompletion(message) {
   const res = await groq.chat.completions.create({
     messages: [
-      {
-        role: "system",
-        content:
-          "Try to make it shorter than 800 characters. Get to the point, dont introduce yourself unless asked or greeted. Cosplay as ana from Overwatch, Don't force Overwatch on your responses, steal her identity only.",
-      },
+      // {
+      //   role: "system",
+      //   content:
+      //     "Try to make it shorter than 800 characters. Get to the point, dont introduce yourself unless asked or greeted. Cosplay as ana from Overwatch, Don't force Overwatch on your responses, steal her identity only.",
+      // },
       {
         role: "user",
         content: message,
